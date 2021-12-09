6b4f258e8ed66b61b11e2eb3309c6bdd3c8e5be7
Louis <accounts@louislouw.me>
2021-12-09 18:21:47 +0200

    check expected save value

--- src/tests/autoSave.spec.ts
+++ src/tests/autoSave.spec.ts
@@ -7,47 +7,62 @@ describe('autoSave', () => {
 
     it('saves when dirty after delay', async () => {
         let saved = 0;
-        const autoUnsub = autoSave(() => { ++saved }, dirty, value, { delay: 1 });
+        let savedValue = -1;
+        const autoUnsub = autoSave((v) => { ++saved; savedValue = v; }, dirty, value, { delay: 1 });
 
         expect(saved).toEqual(0);
+        expect(savedValue).toEqual(-1);
         setValue(1);
         expect(saved).toEqual(0);
+        expect(savedValue).toEqual(-1);
 
         await delay(10);
         expect(saved).toEqual(1);
+        expect(savedValue).toEqual(1);
 
         autoUnsub();
+        expect(saved).toEqual(1); //No extra save on exit!
     })
 
     it('saves when destroying', async () => {
         let saved = 0;
-        const autoUnsub = autoSave(() => { ++saved }, dirty, value, { delay: 50000 });
+        let savedValue = -1;
+        const autoUnsub = autoSave((v) => { ++saved; savedValue = v; }, dirty, value, { delay: 50000 });
 
         expect(saved).toEqual(0);
+        expect(savedValue).toEqual(-1);
         setValue(2);
         expect(saved).toEqual(0);
+        expect(savedValue).toEqual(-1);
 
         autoUnsub();
 
         expect(saved).toEqual(1);
+        expect(savedValue).toEqual(2);
     })
 
     it('does not save if value did not change', async () => {
         let saved = 0;
-        const autoUnsub = autoSave(() => { ++saved }, dirty, value, { delay: 10 });
+        let savedValue = -1;
+        const autoUnsub = autoSave((v) => { ++saved; savedValue = v; }, dirty, value, { delay: 10 });
         setValue(3);
         dirty.reset();
         expect(saved).toEqual(0);
+        expect(savedValue).toEqual(-1);
 
         setValue(2);
         expect(saved).toEqual(0); //Race: should still be zero
+        expect(savedValue).toEqual(-1);
         setValue(3);
         expect(saved).toEqual(0); //Race: should still be zero
+        expect(savedValue).toEqual(-1);
 
         await delay(20);
         expect(saved).toEqual(0); //Should still be zero as value wont be dirty
+        expect(savedValue).toEqual(-1);
 
         autoUnsub();
+        expect(saved).toEqual(0); //No extra save on exit!
     })
 
     function setValue(val: any) {
