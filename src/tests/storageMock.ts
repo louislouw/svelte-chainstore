6f80a92c8b326b645213fe7cd3a94fc86d8d1de8
Louis <accounts@louislouw.me>
2021-12-16 01:00:09 +0200

    improved storage mock

--- src/tests/storageMock.ts
+++ src/tests/storageMock.ts
@@ -6,7 +6,7 @@ export const storageMock = (): Storage => {
         setItem: (key: string, value: string): void => { internalStore[key] = value },
         clear: () => { internalStore = {} },
         key: (index: number): string | null => Object.keys(internalStore)[index],
-        removeItem: (key: string) => { internalStore[key] = null },
+        removeItem: (key: string) => delete internalStore[key],
         length: Object.keys(internalStore).length
     }
 }
